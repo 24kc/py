155f9ab4afc2c31af4479b70d8ad8575987f00b2
24kc <24kc@>
2019-12-29 13:31:50 +0800

    -

--- cpp/001.py
+++ cpp/001.py
@@ -9,15 +9,19 @@ argv = sys.argv
 argc = len(argv)
 
 n = 1
-s = string( ''.join(sys.stdin.readlines()) )
+stdio = True
 b = box()
 
 if argc > 1:
 	try:
 		n = int(argv[1])
 	except ValueError:
+		stdio = False
 		s = string( "Usage:\ncommand | " + argv[0] + " %d" )
 		b = box('=', 'ABC', 'sb ', ' sb')
+
+if stdio:
+	s = string( ''.join(sys.stdin.readlines()) )
 
 b.box(s).multi(n-1)
 
